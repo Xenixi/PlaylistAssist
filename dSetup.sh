969076f7cdbaaa7ba10e14add6591cea01cf78ef
Kobe McManus <kobe.mcmanus@xenixi.com>
2022-04-10 02:36:01 -0400

    final first version

---
+++ dSetup.sh
@@ -0,0 +1,5 @@
+pip install spotipy
+pip install keyboard
+pip install chime
+echo Launching Application...
+python PlAs.py   
---
+++ launch-background.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main() {
+    printf("Launching in background...");
+    system("pythonw PlAs.py");
+    return 0;
+};
