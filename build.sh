44c68c49adde51b423c455e933355d8f085a0671
Kobe McManus <kobe.mcmanus@xenixi.com>
2022-04-16 15:21:16 -0400

    added resources for icons & metadata

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 echo "Building to 'Build/'"
 rm -r Build/
 mkdir Build/
-gcc setup.c -o Build/setup.exe
-gcc launch.c -o Build/launch.exe
-gcc launch-background.c -o Build/launch-background.exe
+windres plas.rc plas.o
+windres plas_ins.rc plas_ins.o
+windres plas_background.rc plas_background.o
+gcc setup.c plas_ins.o -o Build/setup.exe
+gcc launch.c plas.o -o Build/launch.exe
+gcc launch-background.c plas_background.o -o Build/launch-background.exe
+rm plas_ins.o
+rm plas.o
+rm plas_background.o
 cp PlAs.py Build/PlAs.py
 cp README.md Build/README.md
 echo "Build Completed."
